7b2c2310485a9455e0be460592357544a3e80375
captainUlitochka <snailcaptain@outlook.com>
2022-09-10 18:57:16 +0300

    Занятие 1 - Первый тест

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-        System.out.println("HelloWorld");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
