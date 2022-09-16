a7b8397d105657ac6e2ee92b5cacc832ffcc8f3f
captainUlitochka <snailcaptain@outlook.com>
2022-09-16 17:12:31 +0300

    Занятие 2 Лекция 8

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,6 +2,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
@@ -26,14 +27,18 @@ public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
+
          Response response = RestAssured
                  .given()
                  .redirects()
-                 .follow(true)
+                 .follow(false)
+                 .when()
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
-         int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+         response.prettyPrint();
+
+         String locationHeader = response.getHeader("Location");
+         System.out.println(locationHeader);
     }
 
 
