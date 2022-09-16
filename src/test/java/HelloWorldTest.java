3bf1a666eb6ce5a2618cd2cf37ee4172f7efadeb
captainUlitochka <snailcaptain@outlook.com>
2022-09-16 15:55:22 +0300

    Занятие 2 Лекция 5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
     @Test
@@ -23,4 +27,28 @@ public class HelloWorldTest {
                 .andReturn();
         response.body().print();
     }
+
+    @Test
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+    String name = response.get("answer2");
+    if (name == null) {
+        System.out.println("The key 'answer2' is abscent");
+    } else {
+        System.out.println(name);
+    }
+
+        System.out.println(name);
+    }
+
+
+
 }
