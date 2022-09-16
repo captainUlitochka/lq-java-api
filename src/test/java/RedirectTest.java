9a06c117dfc22fe8a39a294c2e0df80631d1cd66
captainUlitochka <snailcaptain@outlook.com>
2022-09-16 22:53:41 +0300

    Занятие 2 Упражнение 6: Редирект (исправление)

--- src/test/java/RedirectTest.java
+++ src/test/java/RedirectTest.java
@@ -8,10 +8,10 @@ public class RedirectTest {
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
-        String locationHeader = response.getHeader("X-Host");
+        String locationHeader = response.getHeader("Location");
         System.out.println(locationHeader);
     }
 }
