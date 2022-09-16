75f7335846ddbb80e5321902a5dcb65a80d79132
captainUlitochka <snailcaptain@outlook.com>
2022-09-16 19:40:27 +0300

    Занятие 2 Упражнение 5: Парсинг JSON

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -55,6 +56,20 @@ public class HelloWorldTest {
                 .andReturn();
 
         responseForCheck.print();
+    }
+
+    @Test
+    public void testRestAssuredEx5() {
+        List<HashMap<String, String>> values = RestAssured
+                .when()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .then()
+                .extract()
+                .jsonPath()
+                .getList("messages");
+        String result = values.get(1).get("message");
+        System.out.println(result);
+
     }
 
 
