b1f30a1a31d48e85b650e2d91be99adc013f8548
captainUlitochka <snailcaptain@outlook.com>
2022-10-02 15:00:31 +0300

    Занятие 2 Упражнение 7: Долгий редирект

---
+++ src/test/java/LongRedirectTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class LongRedirectTest {
+    @Test
+    public void longRedirect() {
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        System.out.println("Start with " + url);
+        int code;
+        do {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(url)
+                    .andReturn();
+            url = response.getHeader("Location");
+            if (url != null) {
+                System.out.println(url);
+            }
+            code = response.getStatusCode();
+        } while (code != 200);
+    }
+}
