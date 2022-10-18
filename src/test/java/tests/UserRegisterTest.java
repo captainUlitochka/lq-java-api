a9af59e19b72cd3b2dd6bee56543a29f74c9dcfc
captainUlitochka <snailcaptain@outlook.com>
2022-10-18 20:27:42 +0300

    Занятие 4 Задача 15 - Тесты на метод user - WIP

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -6,6 +6,8 @@ import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,4 +44,67 @@ public class UserRegisterTest extends BaseTestCase {
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
         Assertions.assertJsonHasField(responseCreateAuth, "id");
     }
+
+    @Test
+    public void testCreateUserWithIncorrectEmail() {
+        String incorrectEmail = "incorrectEmail.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", incorrectEmail);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource
+    public void testCreateUserWithoutPassword() {
+    // Сделать csv, где бы в каждой итерации отсутствовал по очереди один из параметров
+    }
+
+    @Test
+    public void testCreateUserWithShortestName() {
+        String shortestName = "Y";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", shortestName);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+    }
+
+    @Test
+    public void testCreateUserWithLongestName() {
+        String longestName = "This username's name really is Daenerys Stormborn of House Targaryen, the First of Her Name, " +
+                "Queen of the Andals and the First Men, Protector of the Seven Kingdoms, the Mother of Dragons, " +
+                "the Khaleesi of the Great Grass Sea, the Unburnt, the Breaker of Chains";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", longestName);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
+    }
+
 }
